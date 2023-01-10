4a21d1e6e97026c8c36c67622c36cb84eeee1133
Vanesa Hoxha <vh53857@ubt-uni.net>
2023-01-11 00:24:47 +0300

    Movie Page Slider

--- Redeo/wwwroot/js/site.js
+++ Redeo/wwwroot/js/site.js
@@ -11,3 +11,15 @@ $('a.jt').cluetip({
     mouseOutClose: true,
     closePosition: '',
 });
+
+var swiper = new Swiper('.mySwiper', {
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+});
